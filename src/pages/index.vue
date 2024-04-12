cd8b416eedf7558dc9a482658ac20fa576a656e8
Vbarre2 <vincent.barre@edu.univ-fcomte.fr>
2024-04-12 14:30:18 +0200

    section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,4 +5,16 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam fuga vero tempore animi, quisquam beatae asperiores unde incidunt ullam dolore quaerat ipsum quam quo libero expedita minus. Similique, totam dignissimos.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Eum dolorum fugit voluptas! Optio nisi et esse exercitationem sint facere numquam voluptatibus, ratione ipsa. Distinctio aliquid possimus excepturi assumenda voluptatem animi dolore quam! Iure aliquid labore fugiat deserunt! Necessitatibus!</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Repudiandae quibusdam beatae, dolor distinctio laboriosam voluptas! Culpa nesciunt fugiat, praesentium voluptas, temporibus dicta reprehenderit ad, nam autem ab ea recusandae officia corrupti delectus? Enim provident maiores minus corporis tempora.</p>
+  </section>
 </template>
