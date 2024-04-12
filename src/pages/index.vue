725b8b771c7eaf9edc31e8f91539c212ca384dfc
Vbarre2 <vincent.barre@edu.univ-fcomte.fr>
2024-04-12 14:57:58 +0200

    correction

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,34 @@
 
 <script setup lang="ts">
-//
+import { ref } from "vue";
+const sectionOpen = ref(1)
 </script>
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
-  <section>
-    <button class="text-xl">bouton 1</button>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam fuga vero tempore animi, quisquam beatae asperiores unde incidunt ullam dolore quaerat ipsum quam quo libero expedita minus. Similique, totam dignissimos.</p>
+ <section>
+    <button class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</button>
+    <p v-show="sectionOpen === 1">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni assumenda suscipit sequi
+      commodi maiores nam consequatur exercitationem fuga reprehenderit, molestias corporis eos sed
+      incidunt iure blanditiis dolores, facere ratione odio!
+    </p>
   </section>
   <section>
-    <button class="text-xl">bouton 2</button>
-    <p>Eum dolorum fugit voluptas! Optio nisi et esse exercitationem sint facere numquam voluptatibus, ratione ipsa. Distinctio aliquid possimus excepturi assumenda voluptatem animi dolore quam! Iure aliquid labore fugiat deserunt! Necessitatibus!</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</button>
+    <p v-show="sectionOpen === 2">
+      Ipsum commodi quo minus quisquam rem voluptatibus suscipit iusto blanditiis. Vero ab nobis
+      explicabo. Hic corrupti mollitia ducimus facere. Id mollitia soluta quaerat reiciendis dolores
+      harum eligendi quam consequuntur facilis?
+    </p>
   </section>
   <section>
-    <button class="text-xl">bouton 3</button>
-    <p>Repudiandae quibusdam beatae, dolor distinctio laboriosam voluptas! Culpa nesciunt fugiat, praesentium voluptas, temporibus dicta reprehenderit ad, nam autem ab ea recusandae officia corrupti delectus? Enim provident maiores minus corporis tempora.</p>
+    <button class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</button>
+    <p v-show="sectionOpen === 3">
+      In quidem iste consectetur aut, amet ratione corporis temporibus ab est molestias velit facere
+      explicabo repudiandae deserunt sint omnis rerum error ut officiis consequuntur sunt beatae
+      inventore. Illo, eum sunt?
+    </p>
   </section>
+
 </template>
